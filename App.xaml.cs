2ee94e64273db21f408ededcb5f504f5a23e5760
Chizarka <109988356+Chizarka@users.noreply.github.com>
2022-07-25 22:21:26 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ColorTest
+{
+   /// <summary>
+   /// Interaction logic for App.xaml
+   /// </summary>
+   public partial class App
+   {
+   }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ColorTest
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+      {
+         ChangeColor(); //Имитирует смену темы (Спустя 3 секнуды присвоит CBackg-50 другой цвет)
+      }
+
+      async void ChangeColor()
+      {
+         await Task.Delay(3000);
+         Application.Current.Resources["CBackg-50"] = Color.FromRgb(255, 0, 0);
+      }
+   }
+}
